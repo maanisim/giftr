7b7b54dbedf7d6b0813a87292a447adae07f39ac
Sean <seanc23lfc@gmail.com>
2020-04-01 19:41:24 +0100

    updates to backend

--- www/backend.py
+++ www/backend.py
@@ -1,1 +1,36 @@
+# Really basic program for now
+#
+#
 import requests
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
+
+def createAccount():
+    return
+
+
+def forgotPassword():
+    return
+
+
+def accountLogin(username, password):
+    data = {
+        'username': username,
+        'pass': password
+    }
+
+    with requests.Session() as session:
+        post = session.post('LOGIN_URL', data=data)
+        r = session.get('PAGE_URL')
+        print(r.text)
+
+
+url = 'test.com/login?username=test&password=thisispass'
+parsed = urlparse.urlparse(url)
+
+# URL = xxx.yyy/login?username=X&password=X
+if 'login' in parsed.path:
+    username = parsed.query[0]
+    password = parsed.query[1]
+    accountLogin(username, password)
