f4ebf8e923d10056f2a32f8e2fbd08cbaa918c57
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-17 03:57:59 +0100

    Update contact.php

--- www/contact.php
+++ www/contact.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us | Giftr</title>
-    <link rel='icon' href='img/favicon.ico' type='image/x-icon'>
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css'><link rel="stylesheet" href="src/style.css">
-    <link rel="stylesheet" href="src/contact.css">
-
-</head>
-	
 <?php
 	$success = false;
 	if ($_POST["submit"]){
@@ -22,6 +10,18 @@
 		$success = true;
 	}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us | Giftr</title>
+    <link rel='icon' href='img/favicon.ico' type='image/x-icon'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css'><link rel="stylesheet" href="src/style.css">
+    <link rel="stylesheet" href="src/contact.css">
+
+</head>	
 	
 <body>
      <div id="page-container">
