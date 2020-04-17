27467974a03d389aeab1128f2a1aa89ff21dfedc
Sean <seanc23lfc@gmail.com>
2020-04-17 21:56:07 +0100

    fixing register

--- backend/backend.py
+++ backend/backend.py
@@ -21,12 +21,16 @@ mysql = MySQL(app)
 def index():
     if 'loggedin' in session:
         # Already logged in
-        return render_template('home.html', email=session['email'])
+        return render_template('welcome.html', email=session['email'])
     return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/loginapi', methods=['POST', 'GET'])
 def loginapi():
@@ -44,29 +48,28 @@ def loginapi():
             session['loggedin'] = True
             session['id'] = account['user_id']
             session['email'] = account['email']
+            session['username'] = account['username']
             return redirect(url_for('welcome'))
         else:
             msg = 'Incorrect login details!'
             return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    
+@app.route('/registerapi', methods=['POST', 'GET'])
+def registerapi():
+    #CREATING ACCOUNT
     if request.method == 'POST' and 'username' in request.form and 'passw' in request.form and 'email' in request.form:
         msg = ''
         # Check if "username", "password" and "email" POST requests exist
         if request.method == 'POST' and 'username' in request.form and 'passw' in request.form and 'email' in request.form:
             # Create variables for easy access
             username = request.form['username']
-            password = hashlib.sha256(
-            request.form['passw'].encode('utf-8')).hexdigest()
+            password = hashlib.sha256(request.form['passw'].encode('utf-8')).hexdigest()
             email = request.form['email']
 
         # Check if account exists
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            'SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
         # Fetch account
         account = cursor.fetchone()
 
@@ -97,7 +100,7 @@ def logout():
 def welcome():
     print("welcome")
     if 'loggedin' in session:
-        return render_template('welcome.html', email=session['email'])
+        return render_template('welcome.html', username=session['username'])
     else:
         return render_template('index.html')
 
