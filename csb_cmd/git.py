b40bcc071a81522cf89c08b9f93eda6c0da6658b
michalani <michal.anisimow@gmail.com>
2020-04-17 19:44:59 +0100

    bot

--- csb_cmd/git.py
+++ csb_cmd/git.py
@@ -36,17 +36,10 @@ class GitCommands(commands.Cog, name="Git commands"):
 
   @commands.command(help="start backend")
   async def start(self, ctx):
-    if (os.system("python3 /home/web/giftr/backend/backend.py") != 0):
-      await ctx.send("Fail")
-    else:
-      await ctx.send("backend.py started")
-
+    await ctx.send("python3 /home/web/giftr/backend/backend.py")
   @commands.command(help="kill backend")
   async def kill(self, ctx):
-    if (os.system("kill $(ps aux | grep 'backend.py' | awk '{print $2}')") != 0):
-      await ctx.send("Fail")
-    else:
-      await ctx.send("backend.py killed")
+    await ctx.send("kill $(ps aux | grep 'backend.py' | awk '{print $2}')")
 
   @commands.command(help="Updates bot to the current git version")
   async def checkout(self, ctx, branch):
