082ae20f32914b1bb438c725dd94e509725ffb74
Tasbiha <60484197+tchohan1@users.noreply.github.com>
2020-04-18 21:35:21 +0100

    Removed script.js

--- backend/templates/contact.php
+++ backend/templates/contact.php
@@ -83,7 +83,7 @@
              </div>
              </div>
             
-             <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script  src="{{ url_for('static', filename='src/script.js') }}"></script>  
+             <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
          </div>
          <div class="footer">
              <a href="about.html">About Us</a>
