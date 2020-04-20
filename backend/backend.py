5d234032d6924670bf102582263a3ab13fcdbc7b
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-20 04:18:49 +0100

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -102,9 +102,9 @@ def emailsent():
         password = "Tataract52"
 
         context = ssl.create_default_context()
-        #with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            #server.login(sender_email, password)
-            #server.sendmail(sender_email, receiver_email, message)
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
     return render_template('emailSent.html')
 
 @app.route('/loginapi', methods=['POST', 'GET'])
