7849835fa65ea6ee6bc606cd4a08daa0ff669e13
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-20 04:10:48 +0100

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -102,7 +102,7 @@ def emailsent():
         password = "Tataract52"
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        #with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             #server.login(sender_email, password)
             #server.sendmail(sender_email, receiver_email, message)
     return render_template(emailSent.html)
