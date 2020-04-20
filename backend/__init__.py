88a30c7babb696267dd65b8419d65c6d5f760f37
Sean <seanc23lfc@gmail.com>
2020-04-20 14:17:36 +0100

    testing new structure

--- backend/__init__.py
+++ backend/__init__.py
@@ -24,13 +24,5 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
+        
     return app
