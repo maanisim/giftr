8d71711e963de0c180f44893240a89ea5b54034b
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-22 22:06:15 +0100

    Update updateDBValues.py

--- backend/giftr/updateDBValues.py
+++ backend/giftr/updateDBValues.py
@@ -5,15 +5,9 @@ from giftr import app, mysql
 from flask import Flask, redirect, url_for, render_template, request, session, make_response
 from flask_mysqldb import MySQLdb
 import ssl, hashlib, re, datetime, smtplib
-import mysql.connector
 
 def update():
-    connection = mysql.connector.connect(
-          host="localhost",
-          user="myusername",
-          passwd="mypassword"
-          #database = "database"
-    )
+
     crsr = connection.cursor()
     crsr.execute("SELECT * FROM products")
     existingProducts = crsr.fetchall()
