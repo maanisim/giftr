babe1fd05346409a5b1f0e55dbe73250e6de110a
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-22 23:40:03 +0100

    Update routes.py

--- backend/giftr/routes.py
+++ backend/giftr/routes.py
@@ -339,7 +339,6 @@ def contact():
 @app.route('/suggestion')
 def suggestion():
     if 'loggedin' in session:
-        initialise()
         update()
         return render_template('itemSuggestion.html')
     return render_template('index.html')
@@ -460,45 +459,3 @@ def update():
     mysql.connection.commit()
     mysql.connection.close()
     
-def initialise():
-    crsr = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    createValues = """CREATE TABLE productRecValues (
-                product_id INT(10) NOT NULL,
-                age_low INT(3) NOT NULL,
-                age_high INT(3) NOT NULL, 
-                price INT(3) NOT NULL,
-                gender1 INT(3) NOT NULL,
-                gender2 INT(3) NOT NULL,
-                toiletries INT(3) NOT NULL,
-                clothes INT(3) NOT NULL,
-                homeware INT(3) NOT NULL,
-                entertainment INT(3) NOT NULL,
-                consumable INT(3) NOT NULL,
-                sport INT(3) NOT NULL,
-                other INT(3) NOT NULL,
-                PRIMARY KEY(product_id),
-                FOREIGN KEY(product_id) REFERENCES products(product_id)
-                )"""
-    
-    createProfiles = """CREATE TABLE profileRecValues(
-                user_id INT(10) NOT NULL,
-                age_low INT(3) NOT NULL,
-                age_high INT(3) NOT NULL,
-                price INT(3) NOT NULL,
-                gender1 INT(3) NOT NULL,
-                gender2 INT(3) NOT NULL,
-                toiletries INT(3) NOT NULL,
-                clothes INT(3) NOT NULL,
-                homeware INT(3) NOT NULL,
-                entertainment INT(3) NOT NULL,
-                consumable INT(3) NOT NULL,
-                sport INT(3) NOT NULL,
-                other INT(3) NOT NULL,
-                PRIMARY KEY(user_id),
-                FOREIGN KEY(user_id) REFERENCES users(user_id)
-                )"""
-
-    crsr.execute(createValues)
-    #crsr.execute(createProfiles)
-    mysql.connection.commit()
-    mysql.connection.close()
