cf195cad47c8a4691d166dd1933981aaabbf8684
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-23 04:53:20 +0100

    Update routes.py

--- backend/giftr/routes.py
+++ backend/giftr/routes.py
@@ -521,7 +521,7 @@ def Recommendation(currentUser, alreadyRecc):
     neigh = NearestNeighbors(n_neighbors=1)
     neigh.fit(dataValues)
     reccID = neigh.kneighbors(userData, return_distance = False)
-    reccID = reccID[0] + 1
+    reccID = reccID["product_id"]
     recommendationsReq = 2
     while (reccID in alreadyRecc):
         neigh = NearestNeighbors(n_neighbors=recommendationsReq)
