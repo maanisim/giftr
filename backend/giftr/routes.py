956d21d2c50a7b93c65ad09d751bc9dddd6ac7cc
RufusWW <60927404+RufusWW@users.noreply.github.com>
2020-04-23 15:31:25 +0100

    Update routes.py

--- backend/giftr/routes.py
+++ backend/giftr/routes.py
@@ -376,7 +376,7 @@ def suggestion2():
     recommendation = Recommendation(session["id"], alreadyRecc)
     image = "img/p/" + recommendation["photo"]
     session["recommendation"] = recommendation
-    return alreadyRecc
+    return recommendation
     #return render_template('itemSuggestion.html', recommendation=recommendation, image = image)
 ###################################################################
 #Functions for Suggestion
